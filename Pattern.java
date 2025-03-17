ea497e63dc72efbcd5d2415b7969347dbe51646b
sangeetpatna <sangeetpatna@gmail.com>
2025-03-17 21:06:54 +0530

    added two pattern programs.

---
+++ Pattern.java
@@ -0,0 +1,13 @@
+class Pattern
+{
+   public static void main(String[] as)
+   {
+      System.out.println("*");
+      System.out.println("* *");
+      System.out.println("* * *");
+      System.out.println("* * * *");
+      System.out.println("* * * * *");
+      System.out.println("* * * * * *");
+      System.out.println("* * * * * * *");
+   }
+}
---
+++ Pattern1.java
@@ -0,0 +1,13 @@
+class Pattern1
+{
+   public static void main(String[] as)
+   {
+      System.out.println("* * * * * * *");
+      System.out.println("* * * * * *");
+      System.out.println("* * * * *");
+      System.out.println("* * * *");
+      System.out.println("* * *");
+      System.out.println("* *");
+      System.out.println("*");
+   }
+}
